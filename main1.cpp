91177adb80b3f2c64080a54c3e8770e530f4cea5
profknight <ahknight@pipeline.sbcc.edu>
2019-03-19 16:53:48 -0700

    More fun with pointers, the heap

--- main1.cpp
+++ main1.cpp
@@ -1,33 +1,13 @@
 #include <iostream>
 #include <utility>
 
-std::pair<uint8_t *, double> findNearestNeighbor(uint8_t *target,
-                                                 uint8_t **neighbors,
-                                                 size_t neighborCount,
-                                                 size_t neighborSize)  {
-    double nearest = 10000000;
-    for (int i = 0; i < neighborCount; i++) {
-        double distance = 0;
-        for (int j = 0; j < neighborSize; j++) {
-            float m = target[i] - neighbors[i][j];
-            distance += m * m;
-        }
-        nearest = std::min(nearest, distance);
-    }
-}
-
 int main(int argc, char *argv[]) {
-    int array[][3] = {{10, 20, 30}, {40, 50, 60}, {70, 80, 90}};
+    int identity[][3] = {{1,0,0}, {0,1,0}, {0,0,1}};
+    int **identityPtr = new int *[3];
+    identityPtr[0] = new int[3];
+    identityPtr[1] = new int[3];
+    identityPtr[2] = new int[3];
+std::cout << identityPtr[1][1];
 
-    uint8_t **neighbors = new uint8_t*[1000];
-    for (int i = 0; i < 1000; i++) {
-        neighbors[i] = new uint8_t[6];
-        neighbors[i][0] = 'a';
-    }
-
-    for (int i = 0; i < 3; i++) {
-        std::cout << static_cast<int *>(neighbors[i][0]) << std::endl;
-    }
-
-
+    return 0;
 }
