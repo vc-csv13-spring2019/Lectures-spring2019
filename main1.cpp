c016a85c33fb3a990cf3441e24d8500f6f006738
profknight <ahknight@pipeline.sbcc.edu>
2019-03-19 18:04:42 -0700

    Lecture on the Big 6

--- main1.cpp
+++ main1.cpp
@@ -1,13 +1,51 @@
 #include <iostream>
 #include <utility>
 
+struct Implicit {
+
+};
+
+struct Class {
+    int *val;
+public:
+    explicit Class(int i): val(new int) { *val = i; }
+    Class(const Class &other) {
+        *this = other;
+    }
+
+    Class& operator =(const Class &rhs) {
+        if (this != &rhs) { // Always check for self assignment
+            delete this->val;
+            this->val = new int;
+            *this->val = *rhs.val;
+        }
+        return *this;
+    }
+
+//    Class& operator =(int v) {
+//        *val = v;
+//    }
+};
+
 int main(int argc, char *argv[]) {
-    int identity[][3] = {{1,0,0}, {0,1,0}, {0,0,1}};
-    int **identityPtr = new int *[3];
-    identityPtr[0] = new int[3];
-    identityPtr[1] = new int[3];
-    identityPtr[2] = new int[3];
-std::cout << identityPtr[1][1];
+
+    Class c1(1);
+    Class c2(2);
+
+    c2 = c1;
+    *c1.val = 3;
+
+    c2 = Class(5);
+
+    Implicit i;
+    Implicit i2(i);
+    Implicit i3(std::move(i));
+
+    i2 = i3; // Memory leak?
+
+    i = std::move(i3);
+
+    std::cout << *c2.val << std::endl;
 
     return 0;
 }
